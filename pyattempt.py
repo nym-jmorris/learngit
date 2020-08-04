d548db09f007fba9c3c1253a8d19f1ccd4d242ea
John Morris <JMorris@nymets.com>
2020-08-04 12:56:43 -0400

    FizBuzz

--- pyattempt.py
+++ pyattempt.py
@@ -1,4 +1,9 @@
 # trying something with python.
 
-for i in range(0,10):
-    print('Hello, this is line ' + str(i))
+for i in range(0,100):
+    if i%35 ==0:
+        print(str(i)+': Fizbuzz')
+    elif i%7 ==0:
+        print(str(i)+': Fiz')
+    elif i%5==0:
+        print(str(i)+': Buzz')
