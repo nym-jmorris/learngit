980bf7631030b103e2cb4ac7431cf5d434cc7b3d
John Morris <JMorris@nymets.com>
2020-08-17 16:44:28 -0400

    now with ZeroPadding

--- picofoamizilch.py
+++ picofoamizilch.py
@@ -21,6 +21,14 @@ a = randrange(0,10)
 b = randrange(0,10)
 c = randrange(0,10)
 
+a = 1
+b = 2 
+c = 3 
+
+rounds = 1
+
+testStatus = False
+
 def getGuess():
     x = input('Enter your guess: ')
     return x
@@ -32,7 +40,7 @@ def evalGuess(guess):
         if val < 0 or  val > 999:
             print('Please enter a three digit positive integer.\n')
             evalStatus = False
-        else evalStatus = True
+        else: evalStatus = True
 
     except ValueError:
         print("Please enter a three digit positive integer.\n")
@@ -40,7 +48,16 @@ def evalGuess(guess):
     
     return evalStatus
 
+def padGuess(guess):
+    if int(guess)<10:
+        guess = '00' + guess
+    if int(guess)<100:
+        guess = '0' + guess
+    return guess
+
 def testGuess(guess):
+    testStatus = False
+
     if int(guess[0])== a:
         print('Pico')
     if int(guess[1])== b:
@@ -48,25 +65,41 @@ def testGuess(guess):
     if int(guess[2])== c:
         print('Pico')
 
+#FOMI is the idea of the wrong spot.
+# if we match a pico, we don't get a FOMI
+
     if int(guess[0])== b or int(guess[0])== c:
-        print('Fomi')
-    if int(guess[1])== a or int(guess[0])== c:
-        print('Fomi')
-    if int(guess[2])== a or int(guess[0])== b:
-        print('Fomi')
+        if int(guess[0]) != a:
+            print('Fomi')
+    if int(guess[1])== a or int(guess[1])== c:
+        if int(guess[1]) != b:
+            print('Fomi')
+    if int(guess[2])== a or int(guess[2])== b:
+        if int(guess[2]) != c:
+            print('Fomi')
 
-    if int(guess[0])!= a and int(guess[0])!= b and int(guess[0])!= c:
+    '''if (int(guess[0])== b or int(guess[0])== c) and  int(guess[0]) != a :
+        print('Fomi')
+    if (int(guess[1])== a or int(guess[1])== c) and  int(guess[1]) != b :
+        print('Fomi')
+    if (int(guess[2])== a or int(guess[2])== b) and  int(guess[2]) != c :
+        print('Fomi')'''
+
+    if int(guess[0]) != a and int(guess[0]) != b and int(guess[0]) != c:
         print('Zilch')
-    if int(guess[1])!= a and int(guess[1])!= b and int(guess[1])!= c:
+    if int(guess[1]) != a and int(guess[1]) != b and int(guess[1]) != c:
         print('Zilch') 
-    if int(guess[2])!= a and int(guess[2])!= b and int(guess[2])!= c:
+    if int(guess[2]) != a and int(guess[2]) != b and int(guess[2]) != c:
         print('Zilch')
 
-    if guess == a+b+c:
+    if guess == str(a)+str(b)+str(c):
         print('You win!')
-        passStatus = True
+        testStatus = True
 
+    return testStatus
 
-guess = getGuess()
-evalStatus = evalGuess(guess)
-testStatus = testGuess(guess)
+while not testStatus:
+    testStatus = testGuess(getGuess())
+    rounds +=1
+
+print('And it only took you {} rounds!'.format(rounds-1))
