ec14ff9508231181ff5c58edda31c15ee596446d
John Morris <JMorris@nymets.com>
2020-08-26 15:18:54 -0400

    problem 31 done

--- euler_2.py
+++ euler_2.py
@@ -1063,22 +1063,20 @@ numlen = len(str(i))
 
 # How many different ways can £2 be made using any number of coins?
 
-pounds = 0 
-p50    = 0
-p20    = 0
-p10    = 0
-p5     = 0
-p2     = 0
-p1     = 0
-
-from math import floor
+'''
 ways = 0
-iters = 0
 
 needed = 200
 
-for a in range(0,needed/100 + 1):
-    for b in range(needed)
-
-    
-        
+for a in range(0,int(needed/100) + 1):
+    for b in range(0,int((needed-a*100)/50)+1):
+        for c in range(0,int((needed-a*100-b*50)/20)+1):
+            for d in range(0,int((needed-a*100-b*50-c*20)/10)+1):
+                for e in range(0,int((needed-a*100-b*50-c*20-d*10)/5)+1):
+                    for f in range(0,int((needed-a*100-b*50-c*20-d*10-e*5)/2)+1):
+                        for g in range(0,int((needed-a*100-b*50-c*20-d*10-e*5-f*2)/1)+1):
+                            if 100*a + 50*b + 20*c + 10*d + 5*e + 2*f + 1*g == 200:
+                                print('1£: {}, 50p: {} 20p: {} 10p: {} 5p:{} 2p: {} 1p: {}'.format(a,b,c,d,e,f,g))
+                                ways+=1
+print(ways+1) # +1 for the 2£ coin.
+'''
