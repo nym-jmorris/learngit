bcac51b9bb5c19a503b124006e9ea847c402d112
John Morris <JMorris@nymets.com>
2020-09-01 17:07:32 -0400

    working, but wrong answer...

--- euler_2.py
+++ euler_2.py
@@ -1270,10 +1270,15 @@ print('Time 1: {} \nTime 2: {}\nTime 3: {}\nTime 4:{}'.format(time1-time0,time2-
 
 import csv
 
+wins_a = 0
+wins_b = 0
+
 with open('p054_poker.txt', newline='') as f:
     reader = csv.reader(f)
     
     data = list(reader)
+
+f = open('output.txt','w')
 
 def evalofKind(hand):
     khand = []
@@ -1360,11 +1365,10 @@ def evalStraight(hand):
         return 'Straight'
     else: return 'Not Straight'
 
-#results = [[0,0,0,0] for iter in range(len(data))]
-
 # need to return 3 high cards [two pair and tie-breaker]
 def highCard(hand,status):
     hhand = []
+    
     for i in range(0,5):
         str_pos = 3*i
         if hand[str_pos] =='T':
@@ -1381,41 +1385,39 @@ def highCard(hand,status):
 
     hhand.sort()
 
-    if status in ['Straight','Straight Flush']:
+    if status[1] in ['Straight','Straight Flush']:
         return [hhand[4],hhand[3],hhand[2]]
 
-    elif status == 'Flush':
+    elif status[1] == 'Flush':
         return [hhand[4],hhand[3],hhand[2]]
 
-    elif status == 'Full House':
+    elif status[1] == 'Full House':
         if hhand[2] == hhand[0]:
             return [hhand[0],hhand[4],0]
         else: return [hhand[4],hhand[0],0]
 
-    elif status == 'Four of a Kind':
+    elif status[1] == 'Four of a Kind':
         if hhand[0]==hhand[1]:
             return [hhand[0],hhand[4],0]
         else: return[hhand[4],hhand[0],0]
 
-    elif status == 'Three of a Kind':
+    elif status[1] == 'Three of a Kind':
         if hhand[0] == hhand[2]:
             return [hhand[0],hhand[4],hhand[3]]
         elif hhand[1] == hhand[3]:
             return [hhand[2],hhand[4],hhand[0]]
         else: return [hhand[4],hhand[1],hhand[0]]
 
-    elif status == 'Two Pair':
+    elif status[1] == 'Two Pair':
         if hhand[4] == hhand[3] and hhand[2] == hhand[1]:
-            #return[hhand[4],hhand[2],hhand[0]]
-            return [50,50,50]
+            return[hhand[4],hhand[2],hhand[0]]
         elif hhand[4] == hhand[3] and hhand[1] == hhand[0]:
-            #return [hhand[4],hhand[1],hhand[2]]
-            return [40,40,40]
+            return [hhand[4],hhand[1],hhand[2]]
         else: 
-            #return[hhand[3],hhand[1],hhand[4]]
-            return [30,30,30]
+            return[hhand[3],hhand[1],hhand[4]]
+    
 
-    elif status == 'One Pair':
+    elif status[1] == 'One Pair':
         if hhand[4]==hhand[3]:
             return [hhand[4],hhand[2],hhand[1]]
         elif hhand[3]==hhand[2]:
@@ -1423,9 +1425,10 @@ def highCard(hand,status):
         elif hhand[2] == hhand[1]:
             return [hhand[2],hhand[4],hhand[3]]
         else: return [hhand[1],hhand[4],hhand[3]]
+    
     #high cards here
-    #else: return [hhand[4],hhand[3],hhand[2]]
-    else: return [100,100,100]
+    else: return [hhand[4],hhand[3],hhand[2]]
+    
 
 def evalHand(hand):
     if evalFlush(hand)=='Flush' and evalStraight(hand) == 'Straight':
@@ -1447,7 +1450,7 @@ def evalHand(hand):
         else: return [30,'One Pair']
     else: return [0,'High Card']
 
-for d in data[10:15]:
+for d in data:
     hand1 = str(d)[2:16]
     hand2 = str(d)[17:31]
 
@@ -1459,30 +1462,41 @@ for d in data[10:15]:
     high2 = highCard(hand2,handStatus2)
 
     
-    print('Player A holds: {} | {}, {},{},{} high'.format(hand1,handStatus1[1],high1[0],high1[1],high1[2]))
-    print('Player B holds: {} | {}, {},{},{} high\n'.format(hand2,handStatus2[1],high2[0],high2[1],high2[2]))
+    print('Player A holds: {} | {} points, {}, {},{},{} high'.format(hand1,handStatus1[0],handStatus1[1],high1[0],high1[1],high1[2]))
+    print('Player B holds: {} | {} points, {}, {},{},{} high'.format(hand2,handStatus2[0],handStatus2[1],high2[0],high2[1],high2[2]))
 
-    # if evalFlush(hand1) == 'Flush' and evalStraight(hand1) == 'Straight':
-    #     print('This hand {} is a Straight Flush, {} high'.format(hand1,high1[0]))
-    
-    # if evalFlush(hand2) == 'Flush' and evalStraight(hand2) == 'Straight':
-    #     print('This hand {} is a Straight Flush, {} high'.format(hand2,high2[0]))
+    f.write('Player A holds: {} | {} points, {}, {},{},{} high\n'.format(hand1,handStatus1[0],handStatus1[1],high1[0],high1[1],high1[2]))
+    f.write('Player B holds: {} | {} points, {}, {},{},{} high\n'.format(hand2,handStatus2[0],handStatus2[1],high2[0],high2[1],high2[2]))
 
+    if handStatus1[0]>handStatus2[0]:
+        print('Player A wins')
+        wins_a += 1
+    elif handStatus1[0]<handStatus2[0]:
+        print('Player B wins')
+        wins_b += 1
+    if handStatus1[0] == handStatus2[0]:
+        if high1[0]>high2[0]:
+            print('Player A wins')
+            wins_a += 1
+        elif high1[0]<high2[0]:
+            print('Player B wins')
+            wins_b += 1
+        elif high1[1] > high2[1]:
+            print('Player A wins')
+            wins_a += 1
+        elif high1[1]<high2[1]:
+            print('Player B wins')
+            wins_b += 1
+        elif high1[2] > high2[2]:
+            print('Player A wins')
+            wins_a += 1
+        elif high1[2]<high2[2]:
+            print('Player B wins')
+            wins_b += 1
+        else: print('No Winners')
+    print('\n')
 
-    # if(evalFlush(hand1)) == 'Flush':
-    #     print ('This hand {} is a flush, {} high'.format(hand1,high1[0]))
-    # if(evalFlush(hand2)) == 'Flush':
-    #     print ('This hand {} is a flush, {} high'.format(hand2,high2[0]))
-    
-    # if(evalStraight(hand1))== 'Straight':
-    #     print ('This hand {} is a straight, {} high'.format(hand1,high1))
-    # if(evalStraight(hand2))== 'Straight':
-    #     print ('This hand {} is a straight, {} high'.format(hand2,high2))
+print('Player A won {} hands.'.format(wins_a))
+print('Player B won {} hands.'.format(wins_b))
 
-    # handeval = evalofKind(hand1)
-    # if(handeval) is not None:
-    #     print ('This hand {} is a {}, {} high'.format(hand1,handeval,high1))
-    # handeval = evalofKind(hand2)
-    # if(handeval) is not None:
-    #     print ('This hand {} is a {}, {} high'.format(hand2,handeval,high2))
-    
+f.close()
